8ad29efea4bf984f0bea890bdbda19453a489a5e
Muhammad Fahied <fahied@gmail.com>
2013-08-23 16:52:10 +0200

    Initial Commit

---
+++ Hyper Recipes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hyper Recipes
+//
+//  Created by Fahied on 8/23/13.
+//  Copyright (c) 2013 Fahied. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
