1d65ca510e483cb8bcb06d88d5f178ebe7a5562a
Muhammad Fahied <fahied@gmail.com>
2013-08-24 19:25:45 +0200

    addded Reachability Pod 3.1.1
    pod 'Reachability',  '~> 3.1.1'

--- Podfile
+++ Podfile
@@ -1,3 +1,4 @@
 platform :ios, '5.0' 
 pod 'RestKit', '~> 0.20.0'
 pod 'MagicalRecord', '~> 2.1'
+pod 'Reachability',  '~> 3.1.1'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -18,6 +18,12 @@
 #define COCOAPODS_VERSION_MINOR_MagicalRecord 1
 #define COCOAPODS_VERSION_PATCH_MagicalRecord 0
 
+// Reachability
+#define COCOAPODS_POD_AVAILABLE_Reachability
+#define COCOAPODS_VERSION_MAJOR_Reachability 3
+#define COCOAPODS_VERSION_MINOR_Reachability 1
+#define COCOAPODS_VERSION_PATCH_Reachability 1
+
 // RestKit
 #define COCOAPODS_POD_AVAILABLE_RestKit
 #define COCOAPODS_VERSION_MAJOR_RestKit 0
